ae1276da37d1bdcec0d3d56ed4f654100a0fbc2e
Yana T <yana.work.related@gmail.com>
2024-09-20 16:14:45 -0700

    filled in draft for WelcomePage

---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './css/Welcome.css';
+
+function WelcomePage() {
+    return (
+        <div className="welcome-container">
+            <h1>Welcome to Assignment Review App</h1>
+            <div>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+            </div>
+        </div>
+    );
+}
+export default WelcomePage;
